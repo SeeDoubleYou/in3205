433b9ccf5a856d69a6a2f9612fee297fbf159619
drayah <drayah@2d667bd8-17d4-11df-b41a-7d47111d17f9>
2010-03-12 14:33:46 +0000

    Added JUnit test cases for PlayerMoveTest.java -testing eating food, winning game, being killed by monster, moving to empty cells, moving to walls, moving to cell outside borders

--- src/test/java/jpacman/model/PlayerMoveTest.java
+++ src/test/java/jpacman/model/PlayerMoveTest.java
@@ -3,8 +3,10 @@ package jpacman.model;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import jpacman.TestUtils;
 
 import org.junit.Test;
+import com.sun.xml.internal.bind.v2.runtime.reflect.Accessor.GetterSetterReflection;
 
 /**
  * Specialize the general MoveTest test suite to one
@@ -37,7 +39,72 @@ public class PlayerMoveTest extends MoveTest {
         assertTrue(playerMove.invariant());
     }
 
+    @Test
+    /**
+     * Test moving a player to a cell outside the borders of the board
+     */
+    public void testMovingOutsideBorders() {
+    	if (TestUtils.assertionsEnabled()) {
+    		boolean failureGenerated;
+    		boolean movePossible = false;
+    		try {    			
+    			Board board = getTheGame().getBoard();
+    			Cell cellOutside = board.getCell(board.getWidth()+1, board.getHeight()+1);
+    			PlayerMove move = createMove(cellOutside);
+    			movePossible = move.movePossible();
+    			failureGenerated = false;
+    		}
+    		catch (AssertionError ae) {
+    			failureGenerated = true;
+    		}
+    		
+    		assertTrue(failureGenerated);
+    		assertFalse(movePossible);
+    	}
+    }
+    
+    @Test
+    /**
+     * Test player moves
+     */
+    public void testPlayerMoves() {
+    	PlayerMove move = null;
+    	
+    	//a move to a wall should not be possible
+    	move = createMove(getWallCell());
+    	assertFalse(move.movePossible());
+    	assertFalse(move.playerDies());
+    	
+    	//a move to a monster should kill me
+    	move = createMove(getMonsterCell());
+    	assertFalse(move.movePossible());
+    	assertTrue(move.playerDies());
+    	
+    	//a move to an empty cell should be ok
+    	move = createMove(getEmptyCell());
+    	assertTrue(move.movePossible());
+    	assertFalse(move.playerDies());
+    	move.apply();
+    	assertTrue(move.moveDone());
+    	
+    	//eat food
+    	int food = getThePlayer().getPointsEaten();
+    	move = createMove(getFoodCell());
+    	assertTrue(move.movePossible());
+    	assertEquals(1, move.getFoodEaten());
+    	move.apply();
+    	assertTrue(move.moveDone());
+    	assertEquals(food + 1, getThePlayer().getPointsEaten());
+    	
+    	//win the game
+    	Cell lastFood = getTheGame().getBoard().getCell(0, 2);
+    	move = createMove(lastFood);
+    	move.apply();
+    	assertTrue(getTheGame().gameOver());
+    	assertTrue(getTheGame().playerWon());
+    }	
 
+    
     /**
      * Create a move object that will be tested.
      *  @see jpacman.model.MoveTest#createMove(jpacman.model.Cell)
